1bb4bd87ab04cb4b656a32457f7be0f14268def5
Frank Oliveira <fm0325836@gmail.com>
2025-10-21 16:35:53 -0300

    build: Inicial Project Structure

---
+++ src/main/java/com/atvJackson6/atividadePratica6/AtividadePratica6Application.java
@@ -0,0 +1,13 @@
+package com.atvJackson6.atividadePratica6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtividadePratica6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtividadePratica6Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/atvJackson6/atividadePratica6/AtividadePratica6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atvJackson6.atividadePratica6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtividadePratica6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
